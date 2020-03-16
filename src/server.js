1a8db47af379694089df715fa7865cec4f6480c1
Dan Krishnan <dan@wearecodenation.com>
2020-03-16 23:22:49 +0000

    added update and delete task

--- src/server.js
+++ src/server.js
@@ -28,6 +28,19 @@ app.get("/users", async (req, res) => {
   }
 });
 
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    if (!user) {
+      console.log("no user found");
+      return res.status(404).send("No User Found");
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 //task routes
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
@@ -45,6 +58,55 @@ app.get("/tasks", async (req, res) => {
     res.status(200).send(response);
   } catch (error) {
     res.status(404).send(error);
+  }
+});
+
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    if (!task) {
+      console.log("no task found");
+      return res.status(404).send("No task Found");
+    }
+    res.status(200).send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["done"];
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid update" });
+  }
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    updates.forEach(update => (task[update] = req.body[update]));
+
+    await task.save();
+
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(202).send(task);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete({ _id: req.params.id });
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send(task);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
